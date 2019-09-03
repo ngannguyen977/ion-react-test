28c6f7a77e283d13674cc6809bfa296dc77dcf93
Moc Huong Garden <ngannguyen977@gmail.com>
2019-09-03 16:59:59 +0700

    addItem

--- src/components/Test1/Test1.jsx
+++ src/components/Test1/Test1.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import jsonData from "../JsonFiles/test1.json";
 import "./Test1.css";
 
+
 export default class Test1 extends Component {
     constructor(props) {
         super(props);
@@ -10,81 +11,80 @@ export default class Test1 extends Component {
                 columns: [],
                 originalRows: [],
                 rows: [],
-                //search
-               // filtered: [],
             },
-            search: "",
+            query: "",
             type: "",
             question: "",
             error: false,
-            successfull: false
+            successfull: false,
+            // step 1: khoi tao gia tri de luu gia tri nhap trong o search
+            search: "" // luc dau, search bang "" khi e start chuong trinh
         };
         //search
-        this.updateSearch = this.updateSearch.bind(this)
-        
     }
+    addItem(e){
+        e.preventDefault();
+        let newList=[];
+        let list = this.state.list;
+        const newItem = document.getElementById("addInput");
+        const form = document.getElementById("addItemForm");
 
-    updateSearch(event){
-        this.setState({search: event.target.value.substr(0,20)});
+        if(newItem.value !=""){
+            list.push(newItem.value);
+            list.rows = newList;
+            this.setState({
+                list:list
+            });
+        }
     }
-
     componentDidMount() {
         const newData = {
             columns: [...jsonData.columns],
             rows: [...jsonData.rows],
+            originalRows: [...jsonData.rows],
         };
         this.setState({
             data: newData,
-            //search
-            //filtered: this.props.items
         });
     }
-
-    // componentWillReceiveProps(nextProps) {
-    //     this.setState({
-    //         filtered: nextProps.items
-    //     })
-    // }
-
     rows() {
         return this.state.data || [];
     }
-
-    // handleChange(e) {
-    //     let currentList = [];
-    //     let newList = [];
-
-    //     if (e.target.value !== "") {
-    //         currentList = this.props.items;
-    //         newList = currentList.filter(item => {
-    //             const lc = item.toLowerCase();
-    //             const filter = e.target.value.toLowerCase();
-    //         });
-
-    //     } else {
-    //         // If the search bar is empty, set newList to original task list
-    //         newList = this.props.items;
-    //     }
-    //     // Set the filtered state based on what our rules added to newList
-    //     this.setState({
-    //         filtered: newList
-    //     })
-    // }
-    // let getdata=(e)=>{
-    //   return <div>{e.question}</div>
-    // }
-    render() {
-        let filteredItem = this.state.data.rows.filter((item)=>{
-            return item.name.toLowerCase().indexOf(this.state.search) !==-1;
+    //step 2: ham Search de handle input tu o Searchbox
+    search(text) {
+        let newData = this.state.data
+        let listAfterSearch = []
+         // neu e nhap gi do vao o search
+        if (text) {
+            // list sau khi search
+            listAfterSearch = this.state.data.rows.filter(item => (
+                item.order.toString().includes(text)
+                || item.type.includes(text)
+                || item.question.includes(text)
+            ))
+        } else {
+            // neu o search rong, tra lai list ban dau
+            listAfterSearch = this.state.data.originalRows  
+        }
+        // cap nhat lai state 
+        newData.rows = listAfterSearch
+        this.setState({
+            search: text,
+            data: newData
         })
+    }
+
+    render() {
+
         const columns =
             this.state.data.columns &&
             this.state.data.columns.map((col, index) => {
                 return (<th scope="col" key={col.field}> {col.label} </th>
                 );
             });
-        let add = { "order": 9, "type": "product" }
-        this.state.data.rows.push(add)
+            // tam thoi bo di cho e do roi
+        // let add = { "order": 9, "type": "product", "question": "How do I use the mobile remote control service?" }
+        // this.state.data.rows.push(add)
         let rows =
             this.state.data.rows &&
             this.state.data.rows.map(item => {
@@ -95,38 +95,38 @@ export default class Test1 extends Component {
                 </tr>
             })
 
-        //  let a = {ngaysinh: "0109"}
-        //  a.tuoi=20
-        //  a.isMarried = true
+        
         return (<div className="table-wrap coltype"
             style={
                 { width: "50%", margin: "0 auto" }} >
             <h4 > Render a datatable using test1.json </h4>
             <p > Note: the amount of Column can be changed dynamically. </p >
-            <div className="form-group">
+            <form className="form-group">
                 <label> Type </label>
                 <input type="text" className="" />
                 <label> Q & A </label>
-                <input type="name" className="" />
-                <button type="submit"className="btn btn-primary" >Add </button> </div> <br />
+                <input type="name" className=""  id="addInput" />
+                <button type="submit" className="btn btn-primary" onClick ={this.addItem}>Add </button> </form> <br />
             <div className="form-group" >
                 <input type="text"
-                    value={this.state.search}
-                    onChange={() => this.state.updateSearch()}
+                    // step 3: gan cai ham search vs variable search: this.state.search cua e vao day
+                    onChange={e => this.search(e.target.value)} // gan su kien search
+                    value={this.state.search} // gan bien search
                     className="input"
                     placeholder="Search..."
+                    defaultValue=""// them cho khoi warning
                     style={
                         { width: '500' }
                     }
-                /> 
-                <button type="submit"className="btn btn-primary" >Search </button>
+                />
+                <button type="submit" className="btn btn-primary" >Search </button>
             </div > { /* <input type="text" className="form-control m-0" /> */}
             <table className="table ui-accordion" >
-                <caption/>
+                <caption />
                 <colgroup>
-                    <col style={{ width: 10 }}/>
-                    <col style={{ width: 50 } }/> 
-                    <col style={{ width: 30 }}/> 
+                    <col style={{ width: 10 }} />
+                    <col style={{ width: 50 }} />
+                    <col style={{ width: 30 }} />
                 </colgroup >
                 <thead>
                     <tr> {columns} </tr>
